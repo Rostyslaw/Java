e5397ea36c38d2ae1632bc80845bd7e6eca988ea
Rostyslaw <frek2816@gmail.com>
2017-12-06 00:19:54 +0200

    Class_example_static

--- MyProgect1/src/MyClass.java
+++ MyProgect1/src/MyClass.java
@@ -1,9 +1,6 @@
 public class MyClass {
 
     public static void main(String[] args){
-        Vehicle veh = new Vehicle("Blue");
-
-        //veh.setColor("grean");
-        veh.printColor();
+       Vehicle.horn();
     }
 }
--- MyProgect1/src/Vehicle.java
+++ MyProgect1/src/Vehicle.java
@@ -1,20 +1,5 @@
 public class Vehicle {
-    private String color;
-
-    Vehicle(){
-        this.setColor("Red");
-    }
-
-    Vehicle(String c){
-        this.setColor(c);
-    }
-
-    //Setter
-    public void setColor(String c){
-        this.color = c;
-    }
-
-    public void printColor(){
-        System.out.println(color);
+    public static void  horn(){
+        System.out.println("beep");
     }
 }
